11c4e05ee50e06557aabb0ae315f40097192e091
alexborja <gersonperezgerder@gmail.com>
2023-11-19 00:50:10 +0100

    docker problem fixed

---
+++ src/utils/tarjetonType.js
@@ -0,0 +1,19 @@
+export const TARJETON_TYPE = {
+	ggu: {
+		sede: "",
+		pixel: "",
+		linkFinal: "",
+		bannerUrl: "",
+		kw: "",
+		matomo: ""
+	},
+	EU: {
+		subject: "",
+		pixel: "",
+		linkFinal: "",
+		furriel: "",
+		bannerUrl: "",
+		kw: "",
+		matomo: ""
+	}
+}
--- src/views/TarjetonParamsView.jsx
+++ src/views/TarjetonParamsView.jsx
@@ -5,13 +5,23 @@ import { SEDE_MAP } from "../utils/db";
 import Warning from "../components/warning";
 import { INPUT_FIELDS } from "../utils/utils";
 import ParamInput from "../components/paramInput";
+import { TARJETON_TYPE } from "../utils/tarjetonType";
 
 function TarjetonParamsView({ showParams, setShowParams, sharedParams, setSharedParams }) {
 	let [validParams, setValidParams] = useState("");
+	let [tarjetonType, setTarjetonType] = useState("");
 
 	let submitParams = (e) => {
 		e.preventDefault();
 		validateParams();
+	}
+
+	let handleTarjetonType = (e) => {
+		e.preventDefault();
+
+		if(TARJETON_TYPE[tarjetonType]) {
+			return true
+		}
 	}
 
 	let validateParams = () => {
@@ -71,19 +81,36 @@ function TarjetonParamsView({ showParams, setShowParams, sharedParams, setShared
 		))
 	}
 
+	let validTarjetonType = () => {
+		return (
+			tarjetonType ?
+				<form className="paramsForm" onSubmit={(e) => submitParams(e)}>
+					{renderInputFields()}
+					<div className="btns">
+						<button type="submit" className="btn submit">Seguir</button>
+						<button type="button" className="btn clearBtn" onClick={clearInputs}>Limpiar</button>
+					</div>
+				</form>
+				:
+				<form onSubmit={(e) => handleTarjetonType(e)}>
+					<label>Elije el tipo de Tarjeton:</label>
+					<select onChange={(e) => (setTarjetonType(e.target.value))} value={tarjetonType}>
+						<option value="GGU">Generico de Grado Uneatlantico</option>
+						<option value="EU">Evento Uneatlantico</option>
+					</select>
+					<input type="submit" />
+				</form>
+
+		)
+	}
+
 	let renderWarnings = () => {
 		return validParams.length ? <Warning validParams={validParams} setValidParams={setValidParams}></Warning> : <></>
 	}
 
 	return (
 		<div className="tarjetonParams">
-			<form className="paramsForm" onSubmit={(e) => submitParams(e)}>
-				{renderInputFields()}
-				<div className="btns">
-					<button type="submit" className="btn submit">Seguir</button>
-					<button type="button" className="btn clearBtn" onClick={clearInputs}>Limpiar</button>
-				</div>
-			</form>
+			{validTarjetonType()}
 			{renderWarnings()}
 		</div>
 	)
