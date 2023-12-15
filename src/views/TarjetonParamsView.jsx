1d445b8be1d4d2adef03cf8b866944f4c273177c
alexborja <gersonperezgerder@gmail.com>
2023-12-15 16:43:40 +0100

    marge from master

--- src/views/TarjetonParamsView.jsx
+++ src/views/TarjetonParamsView.jsx
@@ -4,116 +4,135 @@ import PropTypes from "prop-types";
 import Warning from "../components/warning";
 import { INPUT_FIELDS } from "../utils/utils";
 import ParamInput from "../components/paramInput";
-import { SEDE_MAP_UNEATLANTICO } from "../utils/db";
-import { hasSede  } from "../utils/utils";
+import { SEDE_MAP_FUNIBER, SEDE_MAP_UNEATLANTICO } from "../utils/db";
+
 
 function TarjetonParamsView({
-  showParams,
-  setShowParams,
-  sharedParams,
-  setSharedParams,
+	showParams,
+	setShowParams,
+	sharedParams,
+	setSharedParams,
 }) {
-  let [validParams, setValidParams] = useState("");
+	let [validParams, setValidParams] = useState("");
 
-  let submitParams = (e) => {
-    e.preventDefault();
-    validateParams();
-  };
+	let submitParams = (e) => {
+		e.preventDefault();
+		validateParams();
+	};
 
-  let validateParams = () => {
-    if (checkValidParams()) {
-      setShowParams(!showParams);
-    }
-  };
+	let validateParams = () => {
+		if (checkValidParams()) {
+			setShowParams(!showParams);
+		}
+	};
 
-  let handleInput = (e) => {
-    let { name, value } = e.target;
-	setSharedParams((prev) => ({...prev, [name]: value.trim()}));
-  };
+	let handleInput = (e) => {
+		let { name, value } = e.target;
+		setSharedParams((prev) => ({ ...prev, [name]: value.trim() }));
+	};
 
-  let checkValidParams = () => {
-    for (const property in sharedParams) {
-      if (sharedParams[property] === "") {
-        setValidParams("Por favor, rellena todos los campos.");
-        return false;
-      }
-    }
+	let checkValidParams = () => {
+		for (const property in sharedParams) {
+			if (sharedParams[property] === "") {
+				setValidParams("Por favor, rellena todos los campos.");
+				return false;
+			}
+		}
 
-    if (!SEDE_MAP_UNEATLANTICO[sharedParams.sede] && hasSede(sharedParams.type)) {
-      setValidParams("La sede que indicastes no existe.");
-      return false;
-    }
-    return true;
-  };
+		if(sharedParams.funiber) {
+			if (!SEDE_MAP_FUNIBER[sharedParams.sede]) {
+				setValidParams("La sede que indicastes no existe.");
+				return false
+			}
+		} else {
+			if(!SEDE_MAP_UNEATLANTICO[sharedParams.sede]) {
+				setValidParams("La sede que indicastes no existe.");
+				return false;
+			}
+		}
 
-  let clearInputs = () => {
-    setSharedParams({
-      sede: "",
-      pixel: "",
-      linkFinal: "",
-      bannerUrl: "",
-      kw: "",
-      matomo: "",
-    });
-  };
+		/*
+		if (!SEDE_MAP_FUNIBER[sharedParams.sede] && hasSede(sharedParams.type)) {
+			setValidParams("La sede que indicastes no existe.");
+			return false;
+		}
 
-  let renderInputFields = () => {
-    let tarjetonParams = INPUT_FIELDS.filter((param) => param.name in sharedParams);
+		if (!SEDE_MAP_UNEATLANTICO[sharedParams.sede] && hasSede(sharedParams.type)) {
+			setValidParams("La sede que indicastes no existe.");
+			return false;
+		}
+		*/
+		return true;
+	};
 
-	return tarjetonParams.map((field) => (
-      <ParamInput
-        key={field.id}
-        name={field.name}
-        label={field.label}
-        id={field.id}
-        placeholder={field.placeholder}
-        onChange={handleInput}
-        value={sharedParams[field.name]}
-      ></ParamInput>
-    ));
-	
-  };
+	let clearInputs = () => {
+		setSharedParams({
+			sede: "",
+			pixel: "",
+			linkFinal: "",
+			bannerUrl: "",
+			kw: "",
+			matomo: "",
+		});
+	};
 
-  let renderWarnings = () => {
-    return validParams.length ? (
-      <Warning
-        validParams={validParams}
-        setValidParams={setValidParams}
-      ></Warning>
-    ) : (
-      <></>
-    );
-  };
+	let renderInputFields = () => {
+		let tarjetonParams = INPUT_FIELDS.filter((param) => param.name in sharedParams);
 
-  return (
-    <div className="tarjetonParams">
-      <form className="paramsForm" onSubmit={(e) => submitParams(e)}>
-        <div className="inputFields">{renderInputFields()}</div>
-        <div className="btns">
-          <div className="btnContainer">
-            <button type="submit" className="btn submit">
-              Seguir
-            </button>
-            <button
-              type="button"
-              className="btn clearBtn"
-              onClick={clearInputs}
-            >
-              Limpiar
-            </button>
-          </div>
-        </div>
-      </form>
-      {renderWarnings()}
-    </div>
-  );
+		return tarjetonParams.map((field) => (
+			<ParamInput
+				key={field.id}
+				name={field.name}
+				label={field.label}
+				id={field.id}
+				placeholder={field.placeholder}
+				onChange={handleInput}
+				value={sharedParams[field.name]}
+			></ParamInput>
+		));
+
+	};
+
+	let renderWarnings = () => {
+		return validParams.length ? (
+			<Warning
+				validParams={validParams}
+				setValidParams={setValidParams}
+			></Warning>
+		) : (
+			<></>
+		);
+	};
+
+	return (
+		<div className="tarjetonParams">
+			<form className="paramsForm" onSubmit={(e) => submitParams(e)}>
+				<div className="inputFields">{renderInputFields()}</div>
+				<div className="btns">
+					<div className="btnContainer">
+						<button type="submit" className="btn submit">
+							Seguir
+						</button>
+						<button
+							type="button"
+							className="btn clearBtn"
+							onClick={clearInputs}
+						>
+							Limpiar
+						</button>
+					</div>
+				</div>
+			</form>
+			{renderWarnings()}
+		</div>
+	);
 }
 
 TarjetonParamsView.propTypes = {
-  showParams: PropTypes.bool,
-  setShowParams: PropTypes.func,
-  sharedParams: PropTypes.object,
-  setSharedParams: PropTypes.func,
+	showParams: PropTypes.bool,
+	setShowParams: PropTypes.func,
+	sharedParams: PropTypes.object,
+	setSharedParams: PropTypes.func,
 };
 
 export default TarjetonParamsView;
