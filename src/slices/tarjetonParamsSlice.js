1ca3463eb9452f97b517a37486b982ff9315cadd
Gerson Borja <gerson.borja@ctdesarrollo-sdr.org>
2024-04-18 15:36:16 +0200

    save params in local storage

--- src/slices/tarjetonParamsSlice.js
+++ src/slices/tarjetonParamsSlice.js
@@ -3,13 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 export const tarjetonParamsSlice = createSlice({
     name: "tarjetonParams",
     initialState: {
-        tarjetonType: "",
-        params: {}
+        tarjetonType: localStorage.getItem("tarjetonType"),
+        params: JSON.parse(localStorage.getItem("params")),
     },
     reducers: {
         update: (state, action) => {
             state.tarjetonType = action.payload.tarjetonType;
             state.params = action.payload.params;
+
+            localStorage.setItem("tarjetonType", JSON.stringify(action.payload.tarjetonType));
+            localStorage.setItem("params", JSON.stringify(action.payload.params));
         },
     }
 })
